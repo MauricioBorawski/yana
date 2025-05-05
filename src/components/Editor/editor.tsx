786065e7beddec44ef6a74aa4b9dbdfc330156a8
Mauricio Borawski <mauricio.borawski@gmail.com>
2025-05-05 17:54:15 -0300

    WIP Migrate to TipTap editor

--- src/components/Editor/editor.tsx
+++ src/components/Editor/editor.tsx
@@ -1,73 +1,27 @@
-import { useEffect, useRef } from "react";
+import { useEditor, EditorContent } from "@tiptap/react";
+import Paragraph from "@tiptap/extension-paragraph";
+import StarterKit from "@tiptap/starter-kit";
 import { clsx } from "clsx";
 import s from "./editor.module.css";
 
-type EditorType = "title" | "body";
+const extensions = [StarterKit];
 
-type Tag = keyof Pick<React.JSX.IntrinsicElements, "div" | "h1">;
+export const Editor = () => {
+  const editor = useEditor({
+    extensions: [
+      ...extensions,
+      Paragraph.configure({
+        HTMLAttributes: {
+          class: clsx(s["editor-body"]),
+        },
+      }),
+    ],
+    editorProps: {
+      attributes: {
+        class: clsx(s.editor),
+      },
+    },
+  });
 
-interface EditorProps {
-  editorType?: EditorType;
-  children?: React.ReactNode;
-  updateValue: (value: string) => void;
-  createBlock?: () => void;
-}
-
-export const Editor = ({
-  editorType = "body",
-  children,
-  updateValue,
-  createBlock,
-}: EditorProps) => {
-  const editorRef = useRef<HTMLDivElement | null>(null);
-
-  const TagOption: Record<EditorType, Tag> = {
-    body: "div",
-    title: "h1",
-  };
-  const Tag = TagOption[editorType];
-
-  const css = clsx([
-    s.editor,
-    editorType === "title" && s["editor-title"],
-    editorType === "body" && s["editor-body"],
-  ]);
-
-  useEffect(() => {
-    if (editorRef.current) {
-      editorRef.current.textContent = children?.toString() || "";
-    }
-  }, [children]);
-
-  return (
-    <Tag
-      id={editorType === "title" ? "editor-title" : "editor-body"}
-      ref={editorRef}
-      contentEditable
-      suppressContentEditableWarning
-      spellCheck
-      className={css}
-      onBlur={() => {
-        if (editorRef.current) {
-          const text = editorRef.current.textContent;
-
-          updateValue(text || "");
-        }
-      }}
-      onKeyDown={(e) => {
-        if (e.key === "Enter") {
-          e.preventDefault();
-          if (createBlock) {
-            createBlock();
-          }
-        }
-
-        if (e.key === "Escape") {
-          if (editorRef.current) {
-            editorRef.current.blur();
-          }
-        }
-      }}
-    />
-  );
+  return <EditorContent editor={editor} />;
 };
--- src/components/Notes/note.tsx
+++ src/components/Notes/note.tsx
@@ -1,104 +1,24 @@
-import { useLayoutEffect, useState } from "react";
 import { Editor } from "../Editor/editor";
-
-import type { TNote } from "../../types";
+import s from "./notes.module.css";
 
 interface NoteProps {
   title?: string | null;
   content?: Array<string>;
 }
 
-export const Note = ({ title, content }: NoteProps) => {
-  const [note, setNote] = useState<TNote>(() => {
-    return {
-      title: title || "",
-      blocks: [
-        {
-          id: "0",
-          content: content ? content[0] : "",
-        },
-      ],
-    };
-  });
-
-  const updateNoteTitle = (value: string) => {
-    setNote((prev) => ({
-      ...prev,
-      title: value,
-    }));
-  };
-
-  const updateNoteContent = (value: string, id: string) => {
-    setNote((prev) => {
-      const blockToEdit = prev.blocks.find((block) => block.id === id);
-
-      if (blockToEdit?.content === value) return prev;
-
-      if (blockToEdit) {
-        blockToEdit.content = value;
-
-        return {
-          ...prev,
-          blocks: [...prev.blocks],
-        };
-      }
-
-      return {
-        ...prev,
-      };
-    });
-  };
-
-  const createNewBlock = () => {
-    setNote((prev) => {
-      return {
-        ...prev,
-        blocks: [
-          ...prev.blocks,
-          {
-            id: prev.blocks.length.toString(),
-            content: "",
-          },
-        ],
-      };
-    });
-  };
-
-  useLayoutEffect(() => {
-    const blocks: NodeListOf<HTMLElement> =
-      document.querySelectorAll("#editor-body");
-
-    if (blocks.length > 1) {
-      const lastElement = blocks[blocks.length - 1];
-
-      if (lastElement.textContent === "") {
-        lastElement.focus();
-      }
-    }
-  }, [note]);
-
+// eslint-disable-next-line no-empty-pattern
+export const Note = ({  }: NoteProps) => {
   return (
-    <main
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: "24px",
-      }}
-    >
-      <Editor editorType="title" updateValue={updateNoteTitle}>
-        {note.title}
-      </Editor>
-      {note.blocks.map((block) => (
-        <Editor
-          key={block.id}
-          updateValue={(value) => {
-            updateNoteContent(value, block.id);
-          }}
-          createBlock={createNewBlock}
-        >
-          {block.content}
-        </Editor>
-      ))}
-    </main>
+    <div className={s["yana-editor"]}>
+      <h1
+        contentEditable
+        suppressContentEditableWarning
+        autoFocus
+        role="input"
+        tabIndex={0}
+        className={s["yana-editor-title"]}
+      />
+      <Editor />
+    </div>
   );
 };
